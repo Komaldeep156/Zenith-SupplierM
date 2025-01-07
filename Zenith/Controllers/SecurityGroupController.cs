c5b3467da28499850603d906e0a09221c3092be8
Komaldeep156 <komaldeepsinghb@gmail.com>
2025-01-07 19:06:40 +0530

    -Add security group subminu -Create security groupController

---
+++ Zenith/Controllers/SecurityGroupController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Zenith.Controllers
+{
+    public class SecurityGroupController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
