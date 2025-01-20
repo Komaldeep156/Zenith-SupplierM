d99b95f1a7a7eda1a77d42128087cb71b59f0295
Komaldeep156 <komaldeepsinghb@gmail.com>
2025-01-20 10:32:22 +0530

    Security group users

---
+++ Zenith.Repository/DomainModels/SecurityGroupUsers.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Zenith.Repository.DomainModels
+{
+    public class SecurityGroupUsers : BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid SecurityGroupId { get; set; }
+        public Guid UserId { get; set; }
+
+        [ForeignKey("SecurityGroupId")]
+        public virtual SecurityGroups SecurityGroup_SecurityGroupId { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual ApplicationUser User { get; set; }
+
+    }
+}
